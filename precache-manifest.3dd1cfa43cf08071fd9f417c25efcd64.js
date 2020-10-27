664b31deb3e978b417b30957752e121aac92f151
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-10-28 03:10:31 +0530

    Updates

---
+++ precache-manifest.3dd1cfa43cf08071fd9f417c25efcd64.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298f03cbabba048b756219aa8296f33f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09ab28c50646d3aa7697",
+    "url": "/static/css/main.ac52b4fa.chunk.css"
+  },
+  {
+    "revision": "3af926fa864126f4ca8e",
+    "url": "/static/js/2.0e223ca5.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.0e223ca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62459181bd7d8949831c",
+    "url": "/static/js/3.2ff419ad.chunk.js"
+  },
+  {
+    "revision": "09ab28c50646d3aa7697",
+    "url": "/static/js/main.1f442644.chunk.js"
+  },
+  {
+    "revision": "72a4c6fbd9f3345c6657",
+    "url": "/static/js/runtime-main.3b31751c.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "8cd86dd6e30dca230722962f1978eab0",
+    "url": "/static/media/grub.live-logo-yellow.8cd86dd6.svg"
+  },
+  {
+    "revision": "3b6b4ff3a6536854af85f162a5c410f0",
+    "url": "/static/media/grub.live-logo.3b6b4ff3.svg"
+  }
+]);
