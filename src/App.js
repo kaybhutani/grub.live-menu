3752c1b22bf853d2b41e2f912cfb6ec74dfc3518
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-10-28 04:03:04 +0530

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -7,27 +7,25 @@ import Navigation from './components/Navigation'
 import PreviewMenu from './components/PreviewMenu'
 import Footer from './components/Footer'
 import {
-  BrowserRouter as Router,
   Switch,
-  Route
-} from "react-router-dom";
+  Route,
+  HashRouter
+} from "react-router-dom"
 
-import { createBrowserHistory } from "history";
-
-let history = createBrowserHistory()
 
 function App() {
+
   return (
     <div className="App">
       <Navigation />
 
-      <Router history={history}>
+      <HashRouter>
         <Switch>
           <Route path='/:menuId'>
             <PreviewMenu />
           </Route>
         </Switch>
-      </Router>
+      </HashRouter>
       <Footer />
     </div>
   );
