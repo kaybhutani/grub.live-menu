1b0c6391d3a1eb874f3e78710bbc0b7d57d2ecf1
Lakshya Khera <lakshya.khera@gmail.com>
2020-12-11 04:52:30 +0530

    fix: Updated menu item styling

--- src/components/PreviewRestaurantMenu.js
+++ src/components/PreviewRestaurantMenu.js
@@ -1,143 +1,147 @@
-import React, { useEffect, useState } from 'react'
-import themes from '../themes.json'
+import React, { useEffect, useState } from "react";
+import themes from "../themes.json";
+import MenuItem from "./MenuItem";
 
 const PreviewRestaurantMenu = (props) => {
-
-  const [restaurantDetails, setRestaruarntDetails] = useState(props.restaurantDetails)
-  const [searchQuery, setSearchQuery] = useState("")
-  const customizedMenu = props.customizedMenu
-  const [theme, setTheme] = useState(themes.Light)
-  useEffect(()=> {
-    if(customizedMenu) {
-      if(restaurantDetails.menu.theme) {
-        if(themes[restaurantDetails.menu.theme.themeName])
-          setTheme(themes[restaurantDetails.menu.theme.themeName])
+  const [restaurantDetails, setRestaruarntDetails] = useState(
+    props.restaurantDetails
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const customizedMenu = props.customizedMenu;
+  const [theme, setTheme] = useState(themes.Light);
+  useEffect(() => {
+    if (customizedMenu) {
+      if (restaurantDetails.menu.theme) {
+        if (themes[restaurantDetails.menu.theme.themeName])
+          setTheme(themes[restaurantDetails.menu.theme.themeName]);
         else {
-          console.log('No theme name found. Reading theme info')
-          setTheme(restaurantDetails.menu.theme)
+          console.log("No theme name found. Reading theme info");
+          setTheme(restaurantDetails.menu.theme);
         }
       }
-   
     }
 
-    if(searchQuery==="") 
-      setRestaruarntDetails(props.restaurantDetails)
+    if (searchQuery === "") setRestaruarntDetails(props.restaurantDetails);
     else {
       // changing state obj to json
-      const tempRestaurantDetails = JSON.parse(JSON.stringify(props.restaurantDetails))
-      
-      const categories = []
+      const tempRestaurantDetails = JSON.parse(
+        JSON.stringify(props.restaurantDetails)
+      );
 
-      tempRestaurantDetails.menu.categories.forEach(category => {
+      const categories = [];
 
-        if(category.title.toLowerCase().includes(searchQuery.toLowerCase().trim())) {
-          categories.push(category)
-        }
-        else {
-          const tempItems = []
-          for(let i =0; i<category.items.length; i++) {
-            if(category.items[i].itemName.toLowerCase().includes(searchQuery.toLowerCase().trim()))
-              tempItems.push(category.items[i])
+      tempRestaurantDetails.menu.categories.forEach((category) => {
+        if (
+          category.title
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase().trim())
+        ) {
+          categories.push(category);
+        } else {
+          const tempItems = [];
+          for (let i = 0; i < category.items.length; i++) {
+            if (
+              category.items[i].itemName
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase().trim())
+            )
+              tempItems.push(category.items[i]);
           }
-          category.items = tempItems
-          if(tempItems.length>0)
-          {
-            categories.push(category)
+          category.items = tempItems;
+          if (tempItems.length > 0) {
+            categories.push(category);
           }
         }
       });
-      tempRestaurantDetails.menu.categories = categories
-      setRestaruarntDetails(tempRestaurantDetails)
+      tempRestaurantDetails.menu.categories = categories;
+      setRestaruarntDetails(tempRestaurantDetails);
     }
-  }, [props.restaurantDetails, restaurantDetails, searchQuery, customizedMenu])
+  }, [props.restaurantDetails, restaurantDetails, searchQuery, customizedMenu]);
 
   // const searchDish = (e) => {
-    
+
   // }
 
   return (
-    <div className='shadow-box' style={
-      customizedMenu?
-      {
-       ...theme
-        
-        // backgroundColor: theme.backgroundColor
-      }:{}
-    }>
+    <div
+      className="shadow-box"
+      style={
+        customizedMenu
+          ? {
+              ...theme,
 
-      <div  style={{textAlign: "center"}}>
-          <h1>
-            {restaurantDetails.logo?
-            (<img src={restaurantDetails.logo} alt='' className='restaurant-logo'></img>)
-            :
-            (<i className='eos-icons' style={{color: 'red', fontSize: '1em', margin: '8px'}}>local_dining</i>)}   
-            {restaurantDetails.restaurantName}</h1>
-            {
-              customizedMenu? 
-              <h4>{restaurantDetails.bio}</h4>
-              : <></>
+              // backgroundColor: theme.backgroundColor
             }
-        </div> 
-    <br></br>
-    
-    <div style={{textAlign: "center"}}>
-      <input type='text' name='search-restaurant' className='form-input' placeholder='Search Menu' onKeyUp={e => setSearchQuery(e.target.value)} ></input>
-    </div>
+          : {}
+      }
+    >
+      <div style={{ textAlign: "center" }}>
+        <h1>
+          {restaurantDetails.logo ? (
+            <img
+              src={restaurantDetails.logo}
+              alt=""
+              className="restaurant-logo"
+            ></img>
+          ) : (
+            <i
+              className="eos-icons"
+              style={{ color: "red", fontSize: "1em", margin: "8px" }}
+            >
+              local_dining
+            </i>
+          )}
+          {restaurantDetails.restaurantName}
+        </h1>
+        {customizedMenu ? <h4>{restaurantDetails.bio}</h4> : <></>}
+      </div>
+      <br></br>
 
-    <br></br>
-    <div>
-      {restaurantDetails.menu.categories.map((element, key) => {
-            return element.title!== "" ?  (
-            
+      <div style={{ textAlign: "center" }}>
+        <input
+          type="text"
+          name="search-restaurant"
+          className="form-input"
+          placeholder="Search Menu"
+          onKeyUp={(e) => setSearchQuery(e.target.value)}
+        ></input>
+      </div>
+
+      <br></br>
+      <div>
+        {restaurantDetails.menu.categories.map((element, key) => {
+          return element.title !== "" ? (
             <div key={key}>
-            <h2>{element.title}</h2>
-            
+              <h2>{element.title}</h2>
 
-            {
-              element.items.length >0 ?
-               (<div>
-                 {
-                restaurantDetails.menu.categories[key].items.map((item, itemKey) => {
-                  return (
-                    <div className='menu-item' key={itemKey}>
-                      <p className='item-name'>{item.itemName}</p>
-                      <p className='item-price'>{item.itemPrice}</p>
-                      {
-                        customizedMenu && item.itemDescription?
-                        <>
-                          <br></br>
-                          <i className='item-description'>{item.itemDescription}</i> 
-                        </>
-                        :
-                        <></>
-                      }
-                      <br></br>
-                      <br></br>  
-                    </div>
-                  
-                  )
-                })
-               }
-               </div>): 
-                (
-                  <div>
-                    
-                  </div>
-                  ) 
-            }
-            
-          
-          </div>):<div key={key}></div>
-          
-        })}  
+              {element.items.length > 0 ? (
+                <div>
+                  {restaurantDetails.menu.categories[key].items.map(
+                    (item, itemKey) => {
+                      return <MenuItem {...item} key={itemKey} />;
+                    }
+                  )}
+                </div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          ) : (
+            <div key={key}></div>
+          );
+        })}
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p style={{ textAlign: "center", margin: "50px 0px 0px 0px" }}>
+        Menu created with{" "}
+        <a style={{ color: "inherit" }} href="www.grub.live">
+          www.grub.live
+        </a>
+      </p>
     </div>
-    <br></br>
-    <br></br>
-    <br></br>
-    <p style={{textAlign: 'center', margin: "50px 0px 0px 0px"}}>Menu created with <a style={{ color: 'inherit'}} href='www.grub.live'>www.grub.live</a></p>
-    </div>
-  )
+  );
+};
 
-}
-
-export default PreviewRestaurantMenu
+export default PreviewRestaurantMenu;
