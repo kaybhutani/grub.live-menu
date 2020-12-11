a2c4a832dda38f7d8981fd7b733822a2d6500d34
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-12-12 01:36:00 +0530

    styling changes

--- src/components/PreviewRestaurantMenu.js
+++ src/components/PreviewRestaurantMenu.js
@@ -76,8 +76,7 @@ const PreviewRestaurantMenu = (props) => {
       }
     >
       <div style={{ textAlign: "center" }}>
-        <h1>
-          {restaurantDetails.logo ? (
+      {restaurantDetails.logo ? (
             <img
               src={restaurantDetails.logo}
               alt=""
@@ -91,6 +90,7 @@ const PreviewRestaurantMenu = (props) => {
               local_dining
             </i>
           )}
+        <h1>
           {restaurantDetails.restaurantName}
         </h1>
         {customizedMenu ? <h4>{restaurantDetails.bio}</h4> : <></>}
@@ -112,13 +112,15 @@ const PreviewRestaurantMenu = (props) => {
         {restaurantDetails.menu.categories.map((element, key) => {
           return element.title !== "" ? (
             <div key={key}>
+              <br></br>
               <h2>{element.title}</h2>
-              <hr style={customizedMenu ? { borderColor: theme.color } : {}} />
+              <hr style={customizedMenu ? { borderColor: theme.color, opacity: '0.8' } : {}} />
+              <br></br>
               {element.items.length > 0 ? (
                 <div>
                   {restaurantDetails.menu.categories[key].items.map(
                     (item, itemKey) => {
-                      return <MenuItem {...item} key={itemKey} />;
+                      return <MenuItem {...item} theme={theme} key={itemKey} />;
                     }
                   )}
                 </div>
