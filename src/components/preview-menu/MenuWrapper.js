e6282fd813b3b7f843e75a319df5afe19b32aa0e
Lakshya Khera <lakshya.khera@gmail.com>
2020-12-18 05:43:49 +0530

    WIP:Wired up categories with browse menu modal

--- src/components/preview-menu/MenuWrapper.js
+++ src/components/preview-menu/MenuWrapper.js
@@ -1,37 +1,40 @@
 import React from "react";
-import MenuItem from './MenuItem'
+import MenuItem from "./MenuItem";
 const MenuWrapper = (props) => {
-
-  const restaurantDetails = props.restaurantDetails
-  const customizedMenu = props.customizedMenu
+  const restaurantDetails = props.restaurantDetails;
+  const customizedMenu = props.customizedMenu;
 
   return (
     <div>
       {restaurantDetails.menu.categories.map((element, key) => {
         return element.title !== "" ? (
           <div key={key}>
-            <h3 id={element.title.replace(' ', '-')}>
+            <h3 id={element.title.trim().toLowerCase().split(" ").join("-")}>
               {element.title}
             </h3>
             <hr></hr>
             {element.items.length > 0 ? (
-              <div className='menu-items'>
-                <table className='category-table'>
-                {/* <tr style={{textAlign: 'left'}}>
+              <div className="menu-items">
+                <table className="category-table">
+                  {/* <tr style={{textAlign: 'left'}}>
                   
                   Use this later to specify head and QTY type( half, full, 500ml etc)
                    <th></th>
                   <th></th> 
                  
                 </tr> */}
-                {restaurantDetails.menu.categories[key].items.map(
-                  (item, itemKey) => {
-                    return <MenuItem {...item} theme={restaurantDetails.menu.theme} key={itemKey} />;
-                  }
-                )}
-  
+                  {restaurantDetails.menu.categories[key].items.map(
+                    (item, itemKey) => {
+                      return (
+                        <MenuItem
+                          {...item}
+                          theme={restaurantDetails.menu.theme}
+                          key={itemKey}
+                        />
+                      );
+                    }
+                  )}
                 </table>
-                
               </div>
             ) : null}
           </div>
@@ -40,7 +43,7 @@ const MenuWrapper = (props) => {
         );
       })}
     </div>
-  )
-}
+  );
+};
 
 export default MenuWrapper;
