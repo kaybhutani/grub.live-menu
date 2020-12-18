f88003aed71d4b42ea937dee9f3090dd48141293
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-12-18 20:07:31 +0530

    fix: use scrollIntoView to scroll without changing url

--- src/components/preview-menu/MenuSectionModal/MenuSectionModal.js
+++ src/components/preview-menu/MenuSectionModal/MenuSectionModal.js
@@ -2,8 +2,18 @@ import React from "react";
 import Popup from "reactjs-popup";
 import styles from "./MenuSectionModal.module.scss";
 export default (props) => {
+
+  const scrollHandler = (category) => {
+    let el = document.getElementById(category.trim().toLowerCase().split(" ").join("-"))  
+    if(el) el.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'center'
+  })
+  }
+
   let content = props.categories.map(({ category, count }, index) => (
-    <a href={`#${category.trim().toLowerCase().split(" ").join("-")}`}>
+    <a onClick = {() => scrollHandler(category)}>
       <div key={index}>
         <span>{category}</span>
         <span>{count}</span>
