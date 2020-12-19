43486b2671f5e36ad71f022172ae8bbcb11b3334
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-12-20 03:48:36 +0530

    update web with web icon

--- src/components/preview-menu/MenuHeader/SocialIcons.js
+++ src/components/preview-menu/MenuHeader/SocialIcons.js
@@ -1,30 +1,32 @@
 import React from "react";
 import Instagram from "../../../assets/images/instagramLogo.svg";
 import Facebook from "../../../assets/images/facebook.svg";
+import WebIcon from "../../../assets/images/globe.svg";
 const SocialIcons = ({ platform, link }) => {
   if (!platform) {
     throw new Error("Platform is required!");
   }
-
+  
   switch (platform) {
     case "facebook":
       return (
         <a href={link}>
-          <img src={Facebook} alt="" />
+          <img src={Facebook} alt="Facebook" />
         </a>
       );
 
     case "instagram":
       return (
         <a href={link}>
-          <img src={Instagram} alt="" />
+          <img src={Instagram} alt="Instagram" />
         </a>
       );
 
     default:
       return (
         <a href={link}>
-          <img alt="Web" />
+          {/* <img href={WebIcon} alt="web" /> */}
+          <i className='eos-icons' style={{verticalAlign: 'middle', padding: '0px 0px 2px 4px', fontSize: 26}}>public</i>
         </a>
       );
   }
