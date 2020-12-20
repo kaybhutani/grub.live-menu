a61f71d8e18f6d1f39768c1fe654ccf18a8cde21
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-12-20 13:38:48 +0530

    remove qr scanner and redirect to grub.live on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,13 @@
-import React from 'react'
-import QrReader from 'react-qr-scanner'
+import React, { useEffect } from 'react'
 
 const Home = () => {
-  const scanHandler = (data) => {
-    
-    if(data!=null) {
-      if(data.includes('glqr.me'))
-        window.location.href = data
-      else
-        alert(`That doesn't look a QR code we generated!`)
-    }
-  }
+  useEffect(()=> {
+    setTimeout(() => window.location = 'https://grub.live', 2000)
+  })
   return (
     <div className='container' style={{textAlign: "center"}}>
-      {/* <h1>Scan QR Code</h1> */}
-      <br></br>
-      <QrReader
-          style={{width: '80%', margin: 'auto'}}
-          
-          facingMode = 'rear'
-          onError = {(error) => console.log(error)}
-          onScan = {(data) => scanHandler(data)}
-          />
+      <h2>Looks like you scanned a wrong QR, redirecting you to <a href='www.grub.live'>www.grub.live</a></h2>
+      
     </div>
   )
 }
