3c5c8040db07bf865b00a7aeff7a69b5d8b5b109
Kartikay Bhutani <kbhutani0001@gmail.com>
2021-01-13 23:45:28 +0530

    send data to api

--- src/components/preview-menu/StayTuned/StayTuned.js
+++ src/components/preview-menu/StayTuned/StayTuned.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import styles from "./StayTuned.module.scss"
+import {apiBaseUrl} from "../../../config.json"
+
 const StayTuned = ({restaurantDetails, showStayTuned, setShowStayTuned, menuId}) => {
 
   const [phoneNumber, setPhoneNumber] = useState(localStorage.getItem('userPhoneNumber'))
   const [emailId, setEmailId] = useState(localStorage.getItem('userEmailId'))
+  
   const submitHandler = () => {
     setShowStayTuned(false)
     localStorage.setItem('userPhoneNumber', phoneNumber)
     localStorage.setItem('userEmailId', emailId)
     localStorage.setItem(menuId, true)
+    fetch(`${apiBaseUrl}/users/add/${menuId}`, {
+      method: 'post',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({"number": phoneNumber, "email": emailId})
+    })
   }
 
   return (
@@ -20,10 +28,16 @@ const StayTuned = ({restaurantDetails, showStayTuned, setShowStayTuned, menuId})
           <div className={styles.popupDiv}
             modal
           >
+            {/* <h2>Offers and more...</h2> */}
           <div className={styles.formDiv}>
-            <h4>Stay tuned with offers, events and more from {restaurantDetails.name}. Enter your details below.</h4>
+            <p className={styles.head}>Enter your details to stay tuned with <b>Exciting Offers, Events</b>
+              and more from <i><b>{restaurantDetails.restaurantName}</b></i> or 
+              <a onClick={e => {e.preventDefault(); setShowStayTuned(false)}}> <u>skip</u></a> for now.</p>
             <div className={styles.formGroup}>
-              <p>Phone Number</p>
+              <p>Phone Number <span style={{color: "red"}}>*</span></p>
+              <select className={styles.formSelect} disabled={true}>
+                <option selected={true}>+91</option>
+              </select>
               <input value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} className={styles.formIput} type="tel" pattern="[0-9]{10}" required name="phoneNumber" placeholder="Eg:   9999999999"></input>
             </div>
             <div className={styles.formGroup}>
